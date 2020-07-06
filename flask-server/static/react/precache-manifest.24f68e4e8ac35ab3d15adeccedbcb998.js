5cebdb20b22a74898737f438d88e7b55b88bb834
Kemoni <58960485+KemoniCodes@users.noreply.github.com>
2020-07-05 21:14:41 -0700

    b

---
+++ flask-server/static/react/precache-manifest.24f68e4e8ac35ab3d15adeccedbcb998.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a134305f8bcfc31d49afdd61f2d1fcfd",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "da93f5d13d5a1b30ad61",
+    "url": "/static/react/css/2.0fa7cd23.chunk.css"
+  },
+  {
+    "revision": "a393110005a21856eded",
+    "url": "/static/react/css/main.de108818.chunk.css"
+  },
+  {
+    "revision": "da93f5d13d5a1b30ad61",
+    "url": "/static/react/js/2.8d8326c6.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "/static/react/js/2.8d8326c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a393110005a21856eded",
+    "url": "/static/react/js/main.65d80308.chunk.js"
+  },
+  {
+    "revision": "0000cf37807d2e7cdbaa",
+    "url": "/static/react/js/runtime-main.c2b06628.js"
+  },
+  {
+    "revision": "aa9501df489c885cce3f31b0fc6234ef",
+    "url": "/static/react/media/food-and-restaurant.aa9501df.png"
+  }
+]);
